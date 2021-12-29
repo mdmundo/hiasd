69c5eade77a98859f90c0930c0fbd467a1bcba15
Edmundo Paulino <manyymoore@gmail.com>
2021-12-28 22:24:55 -0300

    feat: ✨ Enable the dark mode automatically

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useMemo } from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import Router from "./Router";
 
 const App = () => {
-  return <Router />;
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router />
+    </ThemeProvider>
+  );
 };
 
 export default App;
