ebaf3794c7b9987bf59182b1b3ba1b246915dac8
Edmundo Paulino <manyymoore@gmail.com>
2022-01-28 15:10:08 -0300

    style: 💄 Add zoom transition to error

--- src/error/index.js
+++ src/error/index.js
@@ -1,43 +1,56 @@
-import { Typography, Card, CardContent, Container, Button } from "@mui/material";
+import { useState } from "react";
+import { Typography, Card, CardContent, Container, Button, Zoom } from "@mui/material";
 import { navigate } from "@reach/router";
 
-const Error = () => (
-  <Container
-    sx={{
-      marginTop: 2,
-      marginBottom: 2,
-      padding: (3, 2),
-    }}
-    maxWidth="xs"
-  >
-    <Card>
-      <CardContent>
-        <Typography variant="h1" display="block" align="center" gutterBottom>
-          ⚠️
-        </Typography>
-        <Typography variant="h6" display="block" align="center" gutterBottom>
-          Algum erro ocorreu
-        </Typography>
-        <Typography variant="body2" display="block" align="center" gutterBottom>
-          Verifique sua conexão com a internet. Se estiver tudo certo, talvez este erro seja porque atingimos o limite
-          diário de requisições de hinos. Tente novamente mais tarde, amanhã preferencialmente.
-        </Typography>
-        <Typography variant="body2" display="block" align="center" gutterBottom>
-          Note que mesmo sem conexão ainda é possível ouvir os hinos que já foram baixados.
-        </Typography>
-        <Button
-          disableRipple
-          fullWidth
-          sx={{ marginTop: 3 }}
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Voltar
-        </Button>
-      </CardContent>
-    </Card>
-  </Container>
-);
+const Error = () => {
+  const [out, setOut] = useState(false);
+
+  return (
+    <Container
+      sx={{
+        marginTop: 2,
+        marginBottom: 2,
+        padding: (3, 2),
+      }}
+      maxWidth="xs"
+    >
+      <Zoom
+        in={!out}
+        timeout={300}
+        onExited={() => {
+          navigate("/");
+        }}
+      >
+        <Card>
+          <CardContent>
+            <Typography variant="h1" display="block" align="center" gutterBottom>
+              ⚠️
+            </Typography>
+            <Typography variant="h6" display="block" align="center" gutterBottom>
+              Algum erro ocorreu
+            </Typography>
+            <Typography variant="body2" display="block" align="center" gutterBottom>
+              Verifique sua conexão com a internet. Se estiver tudo certo, talvez este erro seja porque atingimos o
+              limite diário de requisições de hinos. Tente novamente mais tarde, amanhã preferencialmente.
+            </Typography>
+            <Typography variant="body2" display="block" align="center" gutterBottom>
+              Note que mesmo sem conexão ainda é possível ouvir os hinos que já foram baixados.
+            </Typography>
+            <Button
+              disableRipple
+              fullWidth
+              sx={{ marginTop: 3 }}
+              onClick={() => {
+                setOut(true);
+              }}
+            >
+              Voltar
+            </Button>
+          </CardContent>
+        </Card>
+      </Zoom>
+    </Container>
+  );
+};
 
 export default Error;
